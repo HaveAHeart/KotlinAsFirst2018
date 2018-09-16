3e0687d1dadf5b3df24a80cb7dbad47816712ead
Alexey <bedrin18@mail.ru>
2018-09-16 23:27:20 +0300

    LengthInMeters fix

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -71,7 +71,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
 fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
-    return ((sagenes * 48) + (arshins * 16) + vershoks) * 4.445
+    return ((sagenes * 48) + (arshins * 16) + vershoks) * 4.445 / 100
 }
 
 /**
