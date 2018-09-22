441c3af45a3361e163e9fbc1b72ac30884dd9182
Alexey <bedrin18@mail.ru>
2018-09-22 16:17:00 +0300

    lesson2task1 complete

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,7 +1,10 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -17,7 +20,11 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val arr = arrayOf(number % 10, (number / 10) % 10, (number / 100) % 10, (number / 1000))
+    if ((arr[0] + arr[1]) == (arr[2] + arr[3])) return true
+    return false
+}
 
 /**
  * Простая
@@ -26,7 +33,12 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    val diagPlus = ((x1 + y1) == (x2 + y2))               //на диагонали y = x + b
+    val diagMinus = (abs(x1 - y1) == abs(x2 - y2))  //на диагонали y = -x + b
+    val horisAndVert = ((x1 == x2) or (y1 == y2))
+    return diagPlus or diagMinus or horisAndVert
+}
 
 
 /**
@@ -35,7 +47,25 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    if (((year % 400 == 0) or (year % 100 != 0)) and (year % 4 == 0) and (month == 2)) return 29
+    when (month) {
+        1 -> return 31
+        2 -> return 28
+        3 -> return 31
+        4 -> return 30
+        5 -> return 31
+        6 -> return 30
+        7 -> return 31
+        8 -> return 31
+        9 -> return 30
+        10 -> return 31
+        11 -> return 30
+        12 -> return 31
+    }
+    return -1
+
+}
 
 /**
  * Средняя
@@ -45,7 +75,10 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    if (sqrt(sqr(x2 - x1) + sqr(y2 - y1)) <= (r2 - r1)) return true
+    return false
+}
 
 /**
  * Средняя
@@ -56,4 +89,9 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    if (((a <= r) and ((b <= s) or (c <= s)))
+            or ((b <= r) and ((a <= s) or (c <= s)))
+            or ((c <= r) and ((a <= s) or (b <= s)))) return true
+    return false
+}
